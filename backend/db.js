bd8f6a9651b4ed27f637f649e0ad0ab9dedf51d6
vikash kumar <vk0812155@gmail.com>
2026-07-12 13:55:56 +0530

    Connected cloud database

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,17 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Vik@9142",
-    database: "ecommerce"
+    host: "sql8.freesqldatabase.com",
+    user: "sql8832828",
+    password: "Y6JAvvsZB2",
+    database: "sql8832828",
+    port: 3306
 });
 
 db.connect((err) => {
-    if(err){
+    if (err) {
         console.log("Database Error:", err);
-    }else{
+    } else {
         console.log("MySQL Connected");
     }
 });
